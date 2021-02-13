6b7253321fe9774a0ce8bf984beb8d55326f1a0d
Magomed Dadagov <grozniy.info@gmail.com>
2021-02-13 15:52:55 +0300

    fix

--- src/main/java/com/example/demo/service/mapper/ProductMapper.java
+++ src/main/java/com/example/demo/service/mapper/ProductMapper.java
@@ -4,9 +4,11 @@ import com.example.demo.domain.entity.Product;
 import com.example.demo.service.dto.CreateProductDTO;
 import com.example.demo.service.dto.ProductDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper extends EntityMapper<ProductDTO, Product> {
 
+    @Mapping(target = "id", ignore = true)
     Product toEntity(CreateProductDTO dto);
 }
